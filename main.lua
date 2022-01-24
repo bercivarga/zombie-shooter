6f90a8ce8d504d8dfd3b6a45d56110917c371aa5
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-01-24 22:11:35 +0100

    move zombies

--- main.lua
+++ main.lua
@@ -10,9 +10,13 @@ function love.load()
   PLAYER.x = love.graphics.getWidth() / 2
   PLAYER.y = love.graphics.getHeight() / 2
   PLAYER.speed = 180
+
+  ZOMBIES = {}
 end
 
 function love.update(dt)
+
+  -- Controls
   if love.keyboard.isDown("d") then
     PLAYER.x = PLAYER.x + PLAYER.speed * dt
   end
@@ -27,6 +31,12 @@ function love.update(dt)
 
   if love.keyboard.isDown("s") then
     PLAYER.y = PLAYER.y + PLAYER.speed * dt
+  end
+
+  -- Zombie movement
+  for i,z in ipairs(ZOMBIES) do
+    z.x = z.x + (math.cos( GETMOUSEANGLE( z.x, z.y, PLAYER.x, PLAYER.y ) ) * z.speed * dt)
+    z.y = z.y + (math.sin( GETMOUSEANGLE( z.x, z.y, PLAYER.x, PLAYER.y ) ) * z.speed * dt)
   end
 end
 
@@ -43,10 +53,38 @@ function love.draw()
     SPRITES.player:getWidth() / 2,
     SPRITES.player:getHeight() / 2
   )
+
+  for i,z in ipairs(ZOMBIES) do
+    love.graphics.draw(
+      SPRITES.zombie,
+      z.x,
+      z.y,
+      GETMOUSEANGLE( z.x, z.y, PLAYER.x, PLAYER.y ),
+      nil,
+      nil,
+      SPRITES.zombie:getWidth() / 2,
+      SPRITES.zombie:getHeight() / 2
+    )
+  end
 end
 
 -- Helpers
 
 function GETMOUSEANGLE(x1, y1, x2, y2)
   return math.atan2( ( y1 - y2 ), ( x1 - x2 ) ) + math.pi
+end
+
+function SPAWNZOMBIE()
+  local zombie = {}
+  zombie.x = math.random(0, love.graphics.getWidth())
+  zombie.y = math.random(0, love.graphics.getHeight())
+  zombie.speed = 180
+
+  table.insert(ZOMBIES, zombie)
+end
+
+function love.keypressed(key)
+  if key == "space" then
+    SPAWNZOMBIE()
+  end
 end
