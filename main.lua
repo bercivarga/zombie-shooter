cd4ff753d527f60bcfd67c832821e34b65586146
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-01-25 21:19:08 +0100

    Add player boudnaries

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 function love.load()
+  math.randomseed(os.time())
+
+  GAMESTATE = 1 -- 1 for menu, 2 for game, 3 for score
+
   SPRITES = {}
   SPRITES.background = love.graphics.newImage('sprites/background.png')
   SPRITES.bullet = love.graphics.newImage('sprites/bullet.png')
@@ -12,25 +16,31 @@ function love.load()
 
   ZOMBIES = {}
   BULLETS = {}
+
+  MAXTIME = 3
+  TIMER = MAXTIME
+
+  GAMEFONT = love.graphics.newFont(40)
 end
 
 function love.update(dt)
+  if GAMESTATE == 2 then
+    -- Controls
+    if love.keyboard.isDown("d") and PLAYER.x <= love.graphics.getWidth() then
+      PLAYER.x = PLAYER.x + PLAYER.speed * dt
+    end
 
-  -- Controls
-  if love.keyboard.isDown("d") then
-    PLAYER.x = PLAYER.x + PLAYER.speed * dt
-  end
+    if love.keyboard.isDown("a") and PLAYER.x >= 0 then
+      PLAYER.x = PLAYER.x - PLAYER.speed * dt
+    end
 
-  if love.keyboard.isDown("a") then
-    PLAYER.x = PLAYER.x - PLAYER.speed * dt
-  end
+    if love.keyboard.isDown("w") and PLAYER.y >= 0 then
+      PLAYER.y = PLAYER.y - PLAYER.speed * dt
+    end
 
-  if love.keyboard.isDown("w") then
-    PLAYER.y = PLAYER.y - PLAYER.speed * dt
-  end
-
-  if love.keyboard.isDown("s") then
-    PLAYER.y = PLAYER.y + PLAYER.speed * dt
+    if love.keyboard.isDown("s") and PLAYER.y <= love.graphics.getHeight() then
+      PLAYER.y = PLAYER.y + PLAYER.speed * dt
+    end
   end
 
   -- Zombie movement
@@ -75,10 +85,26 @@ function love.update(dt)
       table.remove(BULLETS, i)
     end
   end
+
+  if GAMESTATE == 2 then
+    TIMER = TIMER - dt
+    if TIMER <= 0 then
+      SPAWNZOMBIE()
+      TIMER = MAXTIME
+      if MAXTIME >= 0.5 then
+        MAXTIME = MAXTIME - 0.1
+      end
+    end
+  end
 end
 
 function love.draw()
   love.graphics.draw(SPRITES.background, 0, 0)
+
+  if GAMESTATE == 1 then
+    love.graphics.setFont(GAMEFONT)
+    love.graphics.printf("Click to begin", 0, 50, love.graphics.getWidth(), "center")
+  end
 
   love.graphics.draw(
     SPRITES.player,
@@ -104,10 +130,14 @@ function love.draw()
     )
 
     -- Remove all zombies
-    if DISTANCEBETWEEN(z.x, z.y, PLAYER.x, PLAYER.y) < 100 then
+    if DISTANCEBETWEEN(z.x, z.y, PLAYER.x, PLAYER.y) < 30 then
       for index in ipairs(ZOMBIES) do
         ZOMBIES[index] = nil
       end
+      GAMESTATE = 1
+      PLAYER.x = love.graphics.getWidth() / 2
+      PLAYER.y = love.graphics.getHeight() / 2
+      TIMER = MAXTIME
     end
   end
 
@@ -137,10 +167,25 @@ end
 
 function SPAWNZOMBIE()
   local zombie = {}
-  zombie.x = math.random(0, love.graphics.getWidth())
-  zombie.y = math.random(0, love.graphics.getHeight())
-  zombie.speed = 180
+  zombie.x = 0
+  zombie.y = 0
+  zombie.speed = 90
   zombie.dead = false
+
+  local side = math.random(1, 4)
+  if side == 1 then
+    zombie.x = -30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 2 then
+    zombie.x = love.graphics.getWidth() + 30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 3 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = -30
+  elseif side == 4 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = love.graphics.getHeight() + 30
+  end
 
   table.insert(ZOMBIES, zombie)
 end
@@ -163,7 +208,9 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x, y, button)
-  if button == 1 then
+  if button == 1 and GAMESTATE == 2 then
     SPAWNBULLET()
+  elseif button == 1 and GAMESTATE == 1 then
+    GAMESTATE = 2
   end
 end
